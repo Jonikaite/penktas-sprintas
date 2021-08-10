05ac961e35ba8d4b5cd7bb3bb105a262c6257250
lazauninke <sandrajonikaite@gmail.com>
2021-08-10 18:25:52 +0300

    route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +19,11 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+    Route::middleware(['auth'])->group(function () {
+    Route::resource('employee', App\Http\Controllers\EmployeeController::class);
+    Route::resource('project', App\Http\Controllers\ProjectController::class);
+    Route::get('project/{id}/edit', [App\Http\Controllers\CustomerController::class, 'edit'])->name('project.edit');
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home', [App\Http\Controllers\EmployeeController::class, 'index']);
